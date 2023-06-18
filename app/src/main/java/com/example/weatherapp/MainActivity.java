b04e720705e1f489d79fb7929bb3fcdf33f329da
Bhumika Khanna <khannabhumika108@gmail.com>
2023-06-18 16:14:01 +0530

    Third commit

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -9,10 +9,14 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -30,131 +34,126 @@ import java.text.DecimalFormat;
 import java.util.List;
 import java.util.Locale;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
-
 public class MainActivity extends AppCompatActivity {
-    EditText etcity, etcountry;
+    EditText etcity;
     TextView tvResult;
     TextView location;
     private LocationManager locationManager;
-    private int PERMISSION_CODE=1;
+    private int PERMISSION_CODE = 1;
     private String cityName;
-    private final String url="https://api.openweathermap.org/data/2.5/weather";
-            private final String appid="b2beb17abb1073e57447ace0f1614f6d";
-            DecimalFormat df=new DecimalFormat("#.##");
+    private final String url = "https://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "b2beb17abb1073e57447ace0f1614f6d";
+    DecimalFormat df = new DecimalFormat("#.##");
+    boolean cityFound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etcity=findViewById(R.id.etcity);
-        etcountry=findViewById(R.id.etcountry);
-        tvResult=findViewById(R.id.tvResult);
+        etcity = findViewById(R.id.etcity);
 
-        location=findViewById(R.id.location);
+        tvResult = findViewById(R.id.tvResult);
 
-        locationManager=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        location = findViewById(R.id.location);
 
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,android. Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-           ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION},PERMISSION_CODE);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, PERMISSION_CODE);
+
+        } else {
+            Location location1 = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            cityName = getCityName(location1.getLongitude(), location1.getLatitude());
+            if (!cityName.equals("")) {
+                location.setText(cityName);
+                cityFound = true;
+            }
         }
-        Location location1=locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        cityName=getCityName(location1.getLongitude(),location1.getLatitude());
-       if(!cityName.equals("")){
-           location.setText(cityName);
-       }
-
     }
+    String cityname = "Not Found";
+    public String getCityName(double longitude, double latitude) {
 
-    private String getCityName(double longitude,double latitude){
-        String cityName="Not Found";
-        Geocoder gcd=new Geocoder(getBaseContext(), Locale.getDefault());
-        try{
-            List<Address> addresses=gcd.getFromLocation(latitude,longitude,10);
-            for(Address adr:addresses){
-                if(adr!=null){
-                    String city=adr.getLocality();
-                    if(city!=null && !city.equals("")){
-                        cityName=city;
-                    }
-                    else{
-                        Toast.makeText(this,"User City NOt Found, Please manually type the city Name",Toast.LENGTH_SHORT).show();
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = gcd.getFromLocation(latitude, longitude, 10);
+            for (Address adr : addresses) {
+                if (adr != null) {
+                    String city = adr.getLocality();
+                    if (city != null && !city.equals("")) {
+                        cityname = city;
+                        break; // Found the city name, no need to continue iterating
                     }
                 }
             }
-        }catch (IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        return cityName;
+        return cityname;
     }
 
-
     public void getWeatherDetails(View view) {
-        String tempurl="";
-        String city= etcity.getText().toString().trim();
-        String lc=location.getText().toString().trim();
-        String country=etcountry.getText().toString().trim();
-        if(!lc.equals("")){
-            if(!country.equals("")){
-                tempurl=url+"?q="+lc+ ","+country+ "&appid="+appid;
-            }
-            else{
-                tempurl=url+ "?q="+lc+"&appid="+ appid;
-            }
-        }
-        else{
-            if(!country.equals("")){
-                tempurl=url+"?q="+city+ ","+country+ "&appid="+appid;
-            }
-            else{
-                tempurl=url+ "?q="+city+"&appid="+ appid;
-            }
-            StringRequest stringRequest=new StringRequest(Request.Method.POST, tempurl, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-//                    Log.d("response",response);
-                    String output="";
-                    try{
-                        JSONObject jsonResponse =new JSONObject(response);
-                        JSONArray jsonArray=jsonResponse.getJSONArray("weather");
-                        JSONObject jsonObjectWeather=jsonArray.getJSONObject(0);
-                        String description=jsonObjectWeather.getString("description");
-                        JSONObject jsonObjectMain=jsonResponse.getJSONObject("main");
-                        double temp=jsonObjectMain.getDouble("temp")-273.15;
-                        double feels_like=jsonObjectMain.getDouble("feels_like")-273.15;
-                        float pressure=jsonObjectMain.getInt("pressure");
-                        int humidity=jsonObjectMain.getInt("humidity");
-                        JSONObject jsonObjectWind=jsonResponse.getJSONObject("wind");
-                        String wind=jsonObjectWind.getString("speed");
-                        JSONObject jsonObjectSys=jsonResponse.getJSONObject("sys");
-                        String countryName=jsonObjectSys.getString("country");
-                        String cityName=jsonResponse.getString("name");
-                        tvResult.setTextColor(Color.rgb(68,134,199));
-                        output+="Current weather of "+ cityName+"( "+ countryName+" )"
-                                +"\n Temp: "+ df.format(temp)+ "°C"
-                                +"\n feels likes: "+ df.format(feels_like)+ "°C"
-                                +"\n Humidity: "+ humidity+ "%"
-                                +"\n Description: "+ description
-                                +"\n Wind Speed: "+ wind+ "m/s"
-                                +"\n Pressure: "+ pressure+ "hPa";
-                        tvResult.setText(output);
+        String tempurl = url;
+        String tempurl2 = url;
 
-                    }catch (JSONException e){
-                        e.printStackTrace();
-                    }
-                }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    Toast.makeText(getApplicationContext(),error.toString().trim() ,Toast.LENGTH_SHORT).show();
-                }
-            });
-            RequestQueue requestQueue= Volley.newRequestQueue(getApplicationContext());
-            requestQueue.add(stringRequest);
-        }
+        String city = etcity.getText().toString().trim();
 
+
+        if (cityFound && city.isEmpty()) {
+            tempurl += "?q=" + cityname + "&appid=" + appid;
+        }
+        if (!city.isEmpty()) {
+
+                tempurl2 += "?q=" + city + "&appid=" + appid;
+
+        } else {
+
+                tempurl2 += "?q=" + cityName + "&appid=" + appid;
+
+        }
+        if(tempurl2!=null)
+            tempurl = tempurl2;
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, tempurl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                String output = "";
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                    JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                    String description = jsonObjectWeather.getString("description");
+                    JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                    double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                    double feels_like = jsonObjectMain.getDouble("feels_like") - 273.15;
+                    float pressure = jsonObjectMain.getInt("pressure");
+                    int humidity = jsonObjectMain.getInt("humidity");
+                    JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                    String wind = jsonObjectWind.getString("speed");
+                    JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                    String countryName = jsonObjectSys.getString("country");
+                    String cityNames = jsonResponse.getString("name");
+                    tvResult.setTextColor(Color.rgb(68, 134, 199));
+                    output += "Current weather of " + cityNames
+                            + "\n Temp: " + df.format(temp) + "°C"
+                            + "\n feels likes: " + df.format(feels_like) + "°C"
+                            + "\n Humidity: " + humidity + "%"
+                            + "\n Description: " + description
+                            + "\n Wind Speed: " + wind + "m/s"
+                            + "\n Pressure: " + pressure + "hPa";
+                    tvResult.setText(output);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+        requestQueue.add(stringRequest);
     }
 }
